06afa4380eaec41c6ac62cd29a72efbf050c6bbc
Grosspomm <remi1bourguet@gmail.com>
2020-12-10 14:11:23 +0100

    mise en forme

--- src/Drawing.cpp
+++ src/Drawing.cpp
@@ -11,18 +11,19 @@
 // |_|
 
 Drawing::Drawing(const int width, const int height)
-    : width(width), height(height) {
+    : width(width), height(height)
+{
   image.resize(width * height);
 }
 
 Drawing::~Drawing() {}
 
 /* Save image to file "filename" */
-void Drawing::save(std::string filename) {
-
-  if (filename.substr(filename.find_last_of(".") + 1) != "bmp") {
-    throw std::runtime_error(
-        "Drawing ne supporte que l'enregistrement d'images au format bmp");
+void Drawing::save(std::string filename)
+{
+  if (filename.substr(filename.find_last_of(".") + 1) != "bmp")
+  {
+    throw std::runtime_error("Drawing ne supporte que l'enregistrement d'images au format bmp");
   }
 
   createTestImage();
@@ -36,17 +37,21 @@ void Drawing::save(std::string filename) {
 // |_|
 
 /* Set every point of the image to 0 */
-void Drawing::clearImage() {
-  for (std::vector<char>::iterator it = image.begin(); it != image.end();
-       it++) {
+void Drawing::clearImage()
+{
+  for (std::vector<char>::iterator it = image.begin(); it != image.end();it++)
+  {
     *it = 0;
   }
 }
 
 /* Set image to test */
-void Drawing::createTestImage() {
-  for (int j = 0; j < height; j++) {
-    for (int i = 0; i < width; i++) {
+void Drawing::createTestImage()
+{
+  for (int j = 0; j < height; j++)
+  {
+    for (int i = 0; i < width; i++)
+    {
       image[j * width + i] = (i + j) % 256;
     }
   }
